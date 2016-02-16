f37aa8b57d0148283f000890192a481f5fc57cd8
dinnu93 <dinesh.padisetti@gmail.com>
2016-02-16 16:14:03 +0530

    Added capitalize fnction

--- src/style/js/temp.js
+++ src/style/js/temp.js
@@ -8,13 +8,21 @@ function breadCrumbs(url){
     if(i == "Home"){
       result = result.concat("<li><a href='/'>"+ i +"</a></li>"); 
     }else if(i.indexOf(".html") > -1){
-      result = result.concat("<li class='active'>"+ i.split(".")[0].replace("-"," ") +"</li>");
+      result = result.concat("<li class='active'>"+ capitalize(i.split(".")[0].replace("-"," ")) +"</li>");
     }else{
-      result = result.concat("<li>"+ i.replace("-"," ") +"</li>");
+      result = result.concat("<li>"+ capitalize(i.replace("-"," ")) +"</li>");
     }
   });
   result = result + "</ul>";
   return result;
+}
+
+function capitalize(s){
+  var ls = s.split(" ");
+  ls = ls.map(function(i){
+    return i[0].toUpperCase().concat(i.slice(1));
+  });
+  return ls.join(" ");
 }
 
 $('#content').prepend(breadCrumbs(url));
