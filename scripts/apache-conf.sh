72dc7b8d1320d567a1ac7ba18a9267c91cf17185
Raghupathi Kammari <kraghu.mails@gmail.com>
2017-03-27 16:47:01 +0530

    Update apache-conf.sh

--- scripts/apache-conf.sh
+++ scripts/apache-conf.sh
@@ -2,6 +2,6 @@
 #Author : Raghupathi Kammari
 
 #FILE="/root/labs/vlabs-dev-pages/scripts/links-to-google-groups.txt"
-APACHE_VHOST="/etc/apache2/sites-available/default.conf"
+#APACHE_VHOST="/etc/apache2/sites-available/default.conf"
 
-sudo sed -i '/DocumentRoot/r /root/labs/vlabs-dev-pages/scripts/links-to-google-groups.txt' $APACHE_VHOST
+sudo sed -i '/DocumentRoot/r /root/labs/vlabs-dev-pages/scripts/links-to-google-groups.txt' /etc/apache2/sites-available/default.conf
